a3de265e76235d69c7303571e2ffc2ec09f0c4db
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-08-05 07:22:59 +0530

    Check Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,28 @@
+import java.util.*;
+
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to EmployeeWage Problem : ");
+        System.out.println("Welcome to EmployeeWage Computation Program : ");
+        Scanner sc = new Scanner(System.in);
+
+        //Random random = new Random();
+        System.out.println("Enter number : ");
+
+        int n = sc.nextInt();
+        boolean present = false;
+        if(n == 1){
+            System.out.println("Employee is present ");
+              present = true;
+        }else if(n==0){
+            System.out.println("Employee is absent ");
+             present = false;
+        }else{
+            System.out.println("Enter valid number ");
+        }
+
+
+
         
+
     }
 }
