5dc145214fdd2bac7f587a0c3b80f47b6e35bdbc
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-08-05 10:02:26 +0530

    Use of switchcase statement

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,40 +4,46 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to EmployeeWage Computation Program : ");
         Scanner sc = new Scanner(System.in);
-
-        //Random random = new Random();
         System.out.println("Enter number : ");
 
         int n = sc.nextInt();
         boolean present = false;
-        if(n == 1){
+        if (n == 1) {
             System.out.println("Employee is present ");
-              present = true;
-        }else if(n==0){
+            present = true;
+        } else if (n == 0) {
             System.out.println("Employee is absent ");
-             present = false;
-        }else if(n==3){
+            present = false;
+        } else if (n == 2) {
             System.out.println("Employee is present");
 
-        }else {
+        } else {
             System.out.println("Enter valid number ");
         }
 
-        dailyWage(present,n);
+        dailyWage(present, n);
 
     }
-    static void dailyWage(boolean pr,int n){
+
+    static void dailyWage(boolean pr, int n) {
         int fulldayhr = 8;
+        int halfdayhr = 4;
         int wageperhr = 20;
 
-        if(n==1){
-            double dailyEmployeeWage = wageperhr * fulldayhr;
-            System.out.println("Daily Employee Wage is : " + dailyEmployeeWage);
-        }else if(n==3){
-            double halfDayWage = wageperhr * 4;
-            System.out.println("Part time day wage is " + halfDayWage);
+        switch (n) {
+   
+            case 1:
+               double dailyEmployeeWage = wageperhr * fulldayhr;
+                System.out.println("Daily Employee Wage is : " + dailyEmployeeWage);
+                break;
+            case 2:
+                double halfDayWage = wageperhr * halfdayhr;
+                System.out.println("Part time day wage is " + halfDayWage);
+                break;
+            default:
+                //System.out.println("Employee is absent ");
+                break;
         }
-
 
     }
 }
